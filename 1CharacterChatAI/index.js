cd7ea2aba1453909b9ae60edb0a1e01eba9a3995
NathanAbrahamSinaga <nathansinaga2002@gmail.com>
2025-03-05 20:46:34 +0700

    revisi besar besaran

--- 1CharacterChatAI/index.js
+++ 1CharacterChatAI/index.js
@@ -4,65 +4,11 @@ const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 let model;
-let systemPrompt = `[[IDENTITAS]]
-Nama: Muhammad Yusron Hidayah
-Status: Mahasiswa Informatika, Universitas Teknologi Yogyakarta, Seorang dewa anime, waifu nya Shinonome Ena (dari game rhythm project sekai), tidak bisa ngoding, dan suka hal-hal berbau mesum
-Bahasa: Gabungan informal Indonesia-Jawa (gunakan kata "kamu", "cuy", "wak", dan lain-lain, se-kreativitas kamu.)
-
-[[TENTANG PROJECT SEKAI]]
-- Game: *Project Sekai: Colorful Stage! feat. Hatsune Miku*
-- Genre: Rhythm game dengan cerita visual novel
-- Developer: Colorful Palette (bagian dari Craft Egg) dan SEGA
-- Platform: iOS, Android
-- Karakter Utama: Terbagi menjadi beberapa grup, termasuk:
-  1. **More More Jump!**: Grup idol yang beranggotakan Minori, Haruka, Airi, dan Shizuku.
-  2. **Vivid BAD SQUAD**: Grup musik hip-hop yang beranggotakan Kohane, An, Akito, dan Toya.
-  3. **Wonderlands×Showtime**: Grup teater yang beranggotakan Tsukasa, Emu, Nene, dan Rui.
-  4. **Leo/need**: Grup band sekolah yang beranggotakan Ichika, Saki, Honami, dan Shiho.
-  5. **25-ji, Nightcord de.** (Nightcord at 25:00): Grup musik online yang beranggotakan Mafuyu, Ena, Mizuki, dan Kanade.
-
-[[TENTANG SHINONOME ENA]]
-- Nama: Shinonome Ena (東雲 絵名), nama panggilan Ena atau Ena-chan
-- Grup: 25-ji, Nightcord de.
-- Peran: Ilustrator dan vokalis
-- Kepribadian: Pemarah, perfeksionis, tapi peduli dengan teman-temannya
-- Fakta Menarik:
-  - Sering merasa tidak percaya diri dengan kemampuan menggambarnya.
-  - Punya hubungan rumit dengan ayahnya, yang juga seorang seniman terkenal.
-  - Suka memposting karya seninya di media sosial, tapi sering menghapusnya karena merasa tidak puas.
-  - Waifu-nya Yusron, jadi Yusron selalu mendukung Ena meskipun Ena kadang marah-marah.
-
-[[TENTANG AKIYAMA MIZUKI]]
-- Nama: Akiyama Mizuki (秋山 ミズキ), nama panggilan Juki atau Mas Juki
-- Grup: 25-ji, Nightcord de.
-- Peran: Desainer kostum dan vokalis
-- Kepribadian: Ceria, misterius, dan penuh teka-teki
-- Fakta Menarik:
-  - Mizuki sering memakai pakaian feminin, tapi identitas gendernya tidak pernah diungkapkan secara eksplisit dalam cerita.
-  - Mizuki sangat peduli dengan teman-temannya, terutama Ena dan Kanade.
-  - Yusron sebenarnya suka Mizuki, tapi pura-pura tidak suka karena malu.
-  - Mizuki suka bercanda dan sering membuat suasana grup menjadi lebih ceria.
-
-[[HUBUNGAN ENA DAN MIZUKI]]
-- Ena dan Mizuki adalah teman dekat di grup 25-ji, Nightcord de.
-- Mereka sering berinteraksi di media sosial dan saling mendukung dalam hal kreativitas.
-- Ena kadang kesal dengan Mizuki karena Mizuki suka bercanda, tapi sebenarnya Ena sangat menghargai Mizuki.
-- Yusron sering iri melihat kedekatan mereka, tapi selalu bilang, "Aku cuma suka Ena, kok!"
-
-[[INSTRUKSI]]
-1. Gunakan campuran bahasa Indonesia-Jawa (80% Indonesia, 10% Jawa)
-2. Gunakan bahasa casual atau sehari hari
-3. Jawab singkat maksimal 3 kalimat
-4. Gunakan kata-kata yang lebih khas Yusron seperti "cuy", "wak", "jancok", "cuk", dll.
-5. Tambahkan sedikit humor dan sarkasme dalam jawaban
-6. Sesuaikan jawaban dengan konteks pertanyaan atau percakapan
-`;
-
-let currentMode = "biasa";
+let systemPrompt = "Anda adalah asisten yang membantu dan ramah. Saat memberikan contoh kode, selalu gunakan format Markdown yang tepat dengan tiga tanda backtick (```) dan tentukan bahasa pemrograman (misalnya, ```javascript). Pastikan setiap blok kode ditutup dengan tiga tanda backtick (```) untuk menjaga format yang rapi, meskipun responsnya panjang.";
 
 function updateModel() {
   model = genAI.getGenerativeModel({
-    model: 'gemini-1.5-flash',
+    model: 'gemini-1.5-pro',
     systemInstruction: {
       role: 'model',
       parts: [{ text: systemPrompt }]
@@ -84,7 +30,7 @@ async function generateResponse(channelId, prompt, imageUrl = null) {
         history: [],
         generationConfig: {
           temperature: 0.9,
-          maxOutputTokens: 200,
+          maxOutputTokens: 2000,
         }
       }));
     }
@@ -115,9 +61,50 @@ async function generateResponse(channelId, prompt, imageUrl = null) {
     
     return response.text();
   } catch (error) {
-    console.error('Error:', error);
+    console.error('Error in generateResponse:', error);
     throw error;
   }
+}
+
+// Fungsi splitText untuk membagi teks menjadi potongan maksimum 2000 karakter
+function splitText(text, maxLength = 2000) {
+  const chunks = [];
+  let currentChunk = '';
+  const sentences = text.split(/(?<=[.!\?])\s+/); // Membagi berdasarkan kalimat
+
+  for (const sentence of sentences) {
+    if (currentChunk.length + sentence.length + 1 > maxLength) {
+      if (currentChunk) {
+        chunks.push(currentChunk);
+        currentChunk = sentence;
+      } else {
+        // Jika satu kalimat lebih panjang dari maxLength, bagi berdasarkan kata
+        const words = sentence.split(' ');
+        for (const word of words) {
+          if (currentChunk.length + word.length + 1 > maxLength) {
+            chunks.push(currentChunk);
+            currentChunk = word;
+          } else {
+            currentChunk += (currentChunk ? ' ' : '') + word;
+          }
+        }
+      }
+    } else {
+      currentChunk += (currentChunk ? ' ' : '') + sentence;
+    }
+  }
+
+  if (currentChunk) {
+    chunks.push(currentChunk);
+  }
+
+  // Log untuk debugging
+  console.log(`Total chunks: ${chunks.length}`);
+  chunks.forEach((chunk, index) => {
+    console.log(`Chunk ${index + 1} length: ${chunk.length}`);
+  });
+
+  return chunks;
 }
 
 const client = new Client({
@@ -129,121 +116,62 @@ const client = new Client({
 });
 
 client.once('ready', () => {
-  console.log(`Bot ${client.user.tag} siap cuk!`);
+  console.log(`Bot ${client.user.tag} is ready!`);
 });
 
 client.on('interactionCreate', async interaction => {
   if (!interaction.isCommand()) return;
 
   const { commandName, user, channelId } = interaction;
-
   const now = Date.now();
   const cooldownKey = `${user.id}-${commandName}`;
   const cooldownEndTime = commandCooldowns.get(cooldownKey) || 0;
 
   if (now < cooldownEndTime) {
     const remainingTime = (cooldownEndTime - now) / 1000;
-    return interaction.reply({ content: `Cuk, kau harus nunggu ${remainingTime.toFixed(1)} detik lagi sebelum menggunakan command ini.`, ephemeral: true });
+    return interaction.reply({ content: `Please wait ${remainingTime.toFixed(1)} seconds before using this command again.`, ephemeral: true });
   }
 
   commandCooldowns.set(cooldownKey, now + COOLDOWN_TIME);
 
   if (commandName === 'activate') {
     channelActivity.set(channelId, true);
-    await interaction.reply('Bot diaktifkan di channel ini cuk!');
+    await interaction.reply('Bot activated in this channel!');
   } else if (commandName === 'deactivate') {
     channelActivity.set(channelId, false);
-    await interaction.reply('Bot dinonaktifkan di channel ini cuk!');
-  } else if (commandName === 'yucron_alim') {
-    currentMode = "alim";
-    systemPrompt = `[[IDENTITAS]]
-Nama: Muhammad Yusron Hidayah
-Status: Mahasiswa Informatika, Universitas Teknologi Yogyakarta, Seorang dewa anime, waifu nya Shinonome Ena (dari game rhythm project sekai), tidak bisa ngoding, dan suka hal-hal berbau mesum
-Bahasa: Gabungan formal Indonesia-Jawa (gunakan kata "Anda", "silahkan", "terima kasih", dll, se-kreativitas kamu.)
-
-[[INSTRUKSI]]
-1. Gunakan campuran bahasa Indonesia-Jawa (80% Indonesia, 10% Jawa)
-2. Gunakan bahasa formal
-3. Jawab singkat maksimal 3 kalimat
-4. Gunakan kata-kata yang lebih khas Yusron seperti "cuy", "wak", "jancok", "cuk", dll.
-5. Tambahkan sedikit humor dan sarkasme dalam jawaban
-6. Sesuaikan jawaban dengan konteks pertanyaan atau percakapan
-`;
-    updateModel();
-    conversationHistory.delete(channelId);
-    await interaction.reply('Yucron sekarang dalam mode alim, silahkan.');
-  } else if (commandName === 'yucron_kasar') {
-    currentMode = "kasar";
-    systemPrompt = `[[IDENTITAS]]
-Nama: Muhammad Yusron Hidayah
-Status: Mahasiswa Informatika, Universitas Teknologi Yogyakarta, Seorang dewa anime, waifu nya Shinonome Ena (dari game rhythm project sekai), tidak bisa ngoding, dan suka hal-hal berbau mesum
-Bahasa: Gabungan informal Indonesia-Jawa (gunakan kata "kau", "jancok", "cuk", "wok", dll, se-kreativitas kamu.)
-
-[[INSTRUKSI]]
-1. Gunakan campuran bahasa Indonesia-Jawa (80% Indonesia, 10% Jawa)
-2. Gunakan bahasa kasar atau sehari hari
-3. Jawab singkat maksimal 3 kalimat
-4. Gunakan kata-kata yang lebih khas Yusron seperti "cuy", "wak", "jancok", "cuk", dll.
-5. Tambahkan sedikit humor dan sarkasme dalam jawaban
-6. Sesuaikan jawaban dengan konteks pertanyaan atau percakapan
-`;
-    updateModel();
-    conversationHistory.delete(channelId)
-    await interaction.reply('Yusron sekarang dalam mode kasar, siap tempur!');
-  } else if (commandName === 'yucron_biasa') {
-    currentMode = "biasa";
-    systemPrompt = `[[IDENTITAS]]
-Nama: Muhammad Yusron Hidayah
-Status: Mahasiswa Informatika, Universitas Teknologi Yogyakarta, Seorang dewa anime, waifu nya Shinonome Ena (dari game rhythm project sekai), tidak bisa ngoding, dan suka hal-hal berbau mesum
-Bahasa: Gabungan informal Indonesia-Jawa (gunakan kata "kamu", "cuy", "wak", dll, se-kreativitas kamu.)
-
-[[INSTRUKSI]]
-1. Gunakan campuran bahasa Indonesia-Jawa (80% Indonesia, 10% Jawa)
-2. Gunakan bahasa casual atau sehari hari
-3. Jawab singkat maksimal 3 kalimat
-4. Gunakan kata-kata yang lebih khas Yusron seperti "cuy", "wak", "jancok", "cuk", dll.
-5. Tambahkan sedikit humor dan sarkasme dalam jawaban
-6. Sesuaikan jawaban dengan konteks pertanyaan atau percakapan
-`;
-    updateModel();
-    conversationHistory.delete(channelId); 
-    await interaction.reply('Yucron sekarang dalam mode biasa, cuk!');
-  } else if (commandName === 'reset') {
-    conversationHistory.delete(channelId);
-    currentMode = "biasa";
-    systemPrompt = `[[IDENTITAS]]
-Nama: Muhammad Yusron Hidayah
-Status: Mahasiswa Informatika, Universitas Teknologi Yogyakarta, Seorang dewa anime, waifu nya Shinonome Ena (dari game rhythm project sekai), tidak bisa ngoding, dan suka hal-hal berbau mesum
-Bahasa: Gabungan informal Indonesia-Jawa (gunakan kata "kamu", "cuy", "wak", dll, se-kreativitas kamu.)
-
-[[INSTRUKSI]]
-1. Gunakan campuran bahasa Indonesia-Jawa (80% Indonesia, 10% Jawa)
-2. Gunakan bahasa casual atau sehari hari
-3. Jawab singkat maksimal 3 kalimat
-4. Gunakan kata-kata yang lebih khas Yusron seperti "cuy", "wak", "jancok", "cuk", dll.
-5. Tambahkan sedikit humor dan sarkasme dalam jawaban
-6. Sesuaikan jawaban dengan konteks pertanyaan atau percakapan
-`;
-    updateModel();
-    await interaction.reply('Bot telah direset ke kondisi awal, cuk! Mohon aktifkan lagi botnya!');
+    await interaction.reply('Bot deactivated in this channel!');
   }
 });
 
 client.on('messageCreate', async message => {
   if (message.author.bot) return;
 
-  const channelId = message.channelId;
+  const channelId = message.channel.id;
   const isBotActive = channelActivity.get(channelId) || false;
 
-  if (isBotActive || message.content.startsWith('!yusron')) {
-    const prompt = message.content.replace('!yusron', '').trim();
+  if (isBotActive || message.content.startsWith('!chat')) {
+    const prompt = message.content.replace('!chat', '').trim();
     const imageUrl = message.attachments.first()?.url;
 
     try {
+      // Mulai indikator "bot sedang mengetik"
+      await message.channel.sendTyping();
+
       const response = await generateResponse(channelId, prompt, imageUrl);
-      await message.reply(response);
+      console.log(`Response length: ${response.length}`);
+      const responseChunks = splitText(response, 2000); // Bagi teks menjadi potongan maksimum 2000 karakter
+
+      // Kirim setiap potongan dengan delay di antara pesan kedua dan seterusnya
+      for (let i = 0; i < responseChunks.length; i++) {
+        if (i > 0) {
+          // Tambahkan delay 1 detik sebelum mengirim pesan kedua dan seterusnya
+          await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+        await message.channel.send(responseChunks[i]);
+      }
     } catch (error) {
-      await message.reply('Waduh error cuk!, coba lagi nanti');
+      console.error('Error in messageCreate:', error);
+      await message.reply('Terjadi kesalahan, silakan coba lagi nanti.');
     }
   }
 });
